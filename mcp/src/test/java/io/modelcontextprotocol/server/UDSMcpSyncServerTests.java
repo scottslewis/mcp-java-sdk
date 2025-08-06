d5ebbdb591e15957c68e7bb72f41fddafc439536
Scott Lewis <scottslewis@gmail.com>
2025-08-06 15:35:12 -0700

    Added async and sync server tests (UDSMcpAsyncServerTests, UDSMcpSyncServerTest).
    Also made simplifying changes to *socketchannel classes

---
+++ mcp/src/test/java/io/modelcontextprotocol/server/UDSMcpSyncServerTests.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2024-2024 the original author or authors.
+ */
+
+package io.modelcontextprotocol.server;
+
+import java.io.IOException;
+import java.net.UnixDomainSocketAddress;
+import java.nio.file.Files;
+
+import org.junit.jupiter.api.Timeout;
+
+import io.modelcontextprotocol.server.transport.StdioServerTransportProvider;
+import io.modelcontextprotocol.server.transport.UDSServerTransportProvider;
+import io.modelcontextprotocol.spec.McpServerTransportProvider;
+
+/**
+ * Tests for {@link McpSyncServer} using {@link StdioServerTransportProvider}.
+ *
+ * @author Christian Tzolov and Scott Lewis
+ */
+@Timeout(15) // Giving extra time beyond the client timeout
+class UDSMcpSyncServerTests extends AbstractMcpSyncServerTests {
+
+	private UnixDomainSocketAddress address;
+	
+	@Override
+	protected void setUp() {
+		super.onStart();
+		address = UnixDomainSocketAddress.of(getClass().getName()+".unix.socket");
+	}
+	
+	@Override
+	protected void tearDown() {
+		super.onClose();
+		if (address != null) {
+			try {
+				Files.deleteIfExists(address.getPath());
+			} catch (IOException e) {
+			}
+		}
+	}
+	
+	protected McpServerTransportProvider createMcpTransportProvider() {
+			return new UDSServerTransportProvider(address);
+	}
+
+	@Override
+	protected McpServer.SyncSpecification<?> prepareSyncServerBuilder() {
+		return McpServer.sync(createMcpTransportProvider());
+	}
+
+}
